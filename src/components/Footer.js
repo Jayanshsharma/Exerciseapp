fca9f94f5ef596d31b4c9c413afe7848590a6dc2
jayansh sharma <jayanshsharma20@gmail.com>
2023-10-13 00:19:36 +0530

    final2

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,7 +8,7 @@ const Footer = () => {
       <Stack gap="40px" alignItems="center" px="40px" pt="24px">
         <img src={Logo} alt="logo" width="200px" height="40px" />
         <Typography variant='h5' pb="40px" mt="20px">
-          Made with ❤️ by Ujjwal
+          Made by Jayansh Sharma (IIIT Una)
         </Typography>
       </Stack>
     </Box>
